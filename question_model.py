22205d9cfefda211cfe960e4af7e74a8297b844c
Leonardo Jimenez Gonzalez <163687646+LeoJimenezG@users.noreply.github.com>
2024-05-25 14:27:38 -0600

    question format code

---
+++ question_model.py
@@ -0,0 +1,7 @@
+class Question:
+    # Constructor
+    def __init__(self, q_text, q_answer):
+        # The text will be the question
+        self.text = q_text
+        # The answer will be the correct answer to that question
+        self.answer = q_answer
